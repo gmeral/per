7a46bb5b342acde618c22c98758eead32a354553
gmeral <guillaume.meral@etu.u-bordeaux1.fr>
2013-12-13 17:10:38 +0100

    Changement de l'analyse (ajout regexp, hashmaps)

--- fr.labri.demo.analysis/src/fr/labri/demo/analysis/DemoAnalysis.java
+++ fr.labri.demo.analysis/src/fr/labri/demo/analysis/DemoAnalysis.java
@@ -7,6 +7,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import de.lightningbug.api.BugzillaClient;
 import de.lightningbug.api.domain.Bug;
@@ -14,14 +16,20 @@ import de.lightningbug.api.service.BugService;
 import fr.labri.harmony.core.analysis.AbstractAnalysis;
 import fr.labri.harmony.core.config.model.AnalysisConfiguration;
 import fr.labri.harmony.core.dao.Dao;
-import fr.labri.harmony.core.log.HarmonyLogger;
 import fr.labri.harmony.core.model.Author;
 import fr.labri.harmony.core.model.Source;
 
-
+//TODO Hashmap
+//TODO Statistiques
+//TODO API JIRA
+//TODO Bug trouvés non-linkés alors qu'ils existent ?!!
 
 public class DemoAnalysis extends AbstractAnalysis{
-    
+	
+	public static int nbFoundBugs = 0;
+	public static int nbLinkedBugs = 0;
+	
+
 	public DemoAnalysis() {
 		super();		
 	}
@@ -32,16 +40,18 @@ public class DemoAnalysis extends AbstractAnalysis{
 
 	@Override
 	public void runOn(Source src) throws MalformedURLException {
+
+		Map<String, String> bugReport = bugzillaReportExtractor("https://issues.apache.org/bugzilla/", "jjalageas@yahoo.com", "pepsi718");
 		
-		ArrayList<String> bugReport = bugzillaReportExtractor("https://issues.apache.org/bugzilla/", "jjalageas@yahoo.com", "pepsi718");
+		
 		ArrayList<String> links = new ArrayList<String>();
-		
+
 		for (Author auth : src.getAuthors()) {
-			
+
 			for (int i=0; i<auth.getEvents().size(); i++){
-				
+
 				String commitLog = auth.getEvents().get(i).getMetadata().get("commit_message");
-				
+
 				//link search
 				ArrayList<String> link = compareLogToReport(commitLog, bugReport);
 				if(link.size() > 0){
@@ -53,45 +63,56 @@ public class DemoAnalysis extends AbstractAnalysis{
 
 			}
 		}
-		
+
 		for(String s: links)
 			System.out.println(s);
 		System.out.println();
-		
+		System.out.println("Nombre de bugs trouvés : " + nbFoundBugs);
+		System.out.println("Nombre de bugs linkés : " + nbLinkedBugs);
+		System.out.println("Nombre de bugs dans le Bugzilla : " + bugReport.size());
+
 	}
-	
-	
-	public ArrayList<String> compareLogToReport(String commitLog, ArrayList<String> bugReportIds){
-		
-		String delims = "[ , #]+";	
-		String[] logTokens = commitLog.split(delims);
+
+
+	public ArrayList<String> compareLogToReport(String commitLog, Map<String, String> bugReportIds){
+
 		ArrayList<String> linkReport = new ArrayList<String>();
-		
-		for(String log: logTokens)
-			for(String report: bugReportIds)
-				if(log.equals(report))
-					linkReport.add(report);
-	
+		Pattern pattern = Pattern.compile("(\\d){5}");
+		Matcher matcher = pattern.matcher(commitLog);
+
+		while (matcher.find()) {
+			String foundID = matcher.group();
+			System.out.println("Id bug trouve : " + foundID);
+			++nbFoundBugs;
+				if(bugReportIds.containsKey(foundID)) {
+					linkReport.add(foundID);
+					++nbLinkedBugs;
+				}
+
+		}
 		return linkReport;
 	}
-	
-	
-	public ArrayList<String> bugzillaReportExtractor(String bugzillaAddress, String username, String password) throws MalformedURLException{
-		
-		ArrayList<String> bugReport = new ArrayList<String>();
-		final BugzillaClient client = new BugzillaClient(new URL(bugzillaAddress), username, password);
+
+
+	public Map<String, String> bugzillaReportExtractor(String bugzillaAddress, String username, String password) throws MalformedURLException{
+
+		Map<String, String> bugReport = new HashMap<>();
+		BugzillaClient client = new BugzillaClient(new URL(bugzillaAddress), username, password);
 		client.login();
-		final BugService bugService = new BugService(client);
-		final Map<String, Object[]> searchParams = new HashMap<String, Object[]>();
+		BugService bugService = new BugService(client);
+		Map<String, Object[]> searchParams = new HashMap<String, Object[]>();
 		searchParams.put("summary", new Object[]{"ant"});
-		final List<Bug> bugs = bugService.search(searchParams);
-		for(Bug b: bugs)
-			bugReport.add(b.getId().toString());
+		List<Bug> bugs = bugService.search(searchParams);
+		for(Bug b: bugs) {
+			String id = b.getId().toString();
+			bugReport.put(id, id);
+
+		}
 		return bugReport;
 	}
-	
+
 
 }
-	
+
 
 
